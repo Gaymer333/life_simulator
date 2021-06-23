7c9c91828af011b27d81a5265aa82279af7ddb3b
Gaymer333 <elflover333@gmail.com>
2021-06-23 19:37:18 +0200

    Fixed Type in strength

--- src/classes/gameData.tsx
+++ src/classes/gameData.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Stat from "./stat";
 import Time from "./time";
 
-export type StatKeys = "money" | "energy" | "streng"
+export type StatKeys = "money" | "energy" | "strength"
 
 class GameData {
     public time = new Time()
@@ -12,7 +12,7 @@ class GameData {
     private Stats: { [key in StatKeys]: Stat } = {
         money: new Stat({ key: "money", name: "Money", minValue: 0 }),
         energy: new Stat({ key: "energy", name: "Energy", minValue: 0, maxValue: 100, startValue: 100 }),
-        streng: new Stat({ key: "streng", name: "Streng", minValue: 0, maxValue: 100, startValue: 10 }),
+        strength: new Stat({ key: "strength", name: "Strength", minValue: 0, maxValue: 100, startValue: 10 }),
     }
 
     public getStat = (statKey: StatKeys): Stat => {
